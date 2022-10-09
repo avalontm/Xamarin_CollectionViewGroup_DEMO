ce4c4b58c7295b6efdce24d4ee9edd65d0e9ee63
Λv∂lΘи ™ <jaimeraulmendezlopez325@hotmail.com>
2022-10-09 11:06:07 -0700

    updated

---
+++ XamarinDEMO/XamarinDEMO/Sources/ScreenManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace XamarinDEMO.Sources
+{
+    public static class ScreenManager
+    {
+        public static double Width 
+        { 
+            get {  return DeviceDisplay.MainDisplayInfo.Width - 60;  } 
+        }
+    }
+}
